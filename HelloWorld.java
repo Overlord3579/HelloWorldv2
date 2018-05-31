d9deae713abd2d45f1fb49c8294c893530c21a19
Riley Hogue <hogueriley613@gmail.com>
2018-05-31 08:46:28 -0400

    Added basic HelloWorld Code

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+Riley H
+Period 2
+Honers Java
+Hello World Demo Example
+*/
+public class HelloWorld
+{
+   public static void main(String[]args)
+   {
+      System.out.println("Hello World!");
+   }
+}
